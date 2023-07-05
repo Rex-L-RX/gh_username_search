fc0b208ab164c63bd8b28ec8679a256b5252ae9d
Rex-L-RX <liurunxiao233@gmail.com>
2023-07-05 22:33:51 +1000

    practice fetch function to send request

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,25 +1,35 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
 import PubSub from 'pubsub-js'
 export default class Search extends Component {
-    search=()=>{
+    search=async ()=>{
       
         //get user's input
         const {value:keyword}=this.keyWordElement;
-        //updateAppState before sending request
-        PubSub.publish('msg',{isFirst:false,isLoading:true})
-        //send network request
-        axios.get(`https://api.github.com/search/users?q=${keyword}`).then(
-            response=>{
-              //update App state after getting response from server
-              PubSub.publish('msg',{isLoading:false, users:response.data.items})
-            },
-            //update App state after failing to get response from server
-            error=>{
-              PubSub.publish('msg',{isLoading:false, err:error.message})
+        
+        // //updateAppState before sending request
+         PubSub.publish('msg',{isFirst:false,isLoading:true})
+        // //send network request by axios
+        // axios.get(`https://api.github.com/search/users?q=${keyword}`).then(
+        //     response=>{
+        //       //update App state after getting response from server
+        //       PubSub.publish('msg',{isLoading:false, users:response.data.items})
+        //     },
+        //     //update App state after failing to get response from server
+        //     error=>{
+        //       PubSub.publish('msg',{isLoading:false, err:error.message})
               
-            }
-        )
+        //     }
+        // )
+        // use fetch to send request
+        try{
+          const response = await fetch(`https://api.github.com/search/users?q=${keyword}`)
+          const data = await response.json()
+          PubSub.publish('msg',{isLoading:false, users:data.items})
+        } catch(error){
+          PubSub.publish('msg',{isLoading:false, err:error.message})
+        }
+
     }
   render() {
     return (
